defd3ed10f0bacef0ef39406375ad76bcbfd2c90
thatisamood <50476051+CodeContributes@users.noreply.github.com>
2020-05-26 23:35:02 -0400

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+while True:
+  d =["rock", "paper", "scissors"]
+  de = random.choice(d)
+
+  choice = input("Do you choose rock, paper, or scissors? ")
+
+  if de == choice :
+    print("You and the computer have the same answer! Try again.")
+  elif de == "rock" and choice == "paper":
+    print("The computer chose rock. You win!")
+  elif de == "rock" and choice == "scissors":
+    print("The computer chose rock. You lose!")
+  elif de == "paper" and choice == "scissors":
+    print("The computer chose paper. You win! ")
+  elif de == "paper" and choice == "rock":
+    print("The computer chose paper. You lose!")
+  elif de == "scissors" and choice == "rock":
+    print("The computer chose scissors. You win!")
+  elif de == "scissors" and choice == "paper":
+    print("The computer chose scissors. You lose!")
+
+  print("Do you want to restart ? Yes or No")
+  response = input()
+  if not response == "Yes":
+    break
